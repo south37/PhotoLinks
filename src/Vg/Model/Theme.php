61b2940ed4886b0d685e609879ece9e1799a46b0
NewWife <wega315@gmail.com>
2013-08-26 13:50:53 +0900

    Vg/Model/*に工程1で必要なクラスを作成.

---
+++ src/Vg/Model/Theme.php
@@ -0,0 +1,22 @@
+<?php
+namespace Vg\Model;
+
+class Theme 
+{
+    public $id;
+    public $user_id;
+    public $title;
+    public $fix_num;
+    public $frame_num;
+
+    public function __construct()
+    {
+    }
+
+    public function setProperties($data)
+    {
+        foreach (array('id', 'user_id', 'title', 'fix_num', 'frame_num') as $property) {
+            $this->{$property} = (isset($data[$property]))? $data[$property]: "";
+        }
+    }
+}
