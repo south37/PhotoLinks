1b58c0f300da37ae288877cadb42dec9e1073c9a
nantyoku <syukatsusouth@gmail.com>
2013-08-26 10:59:37 +0900

    splitted others.php

--- src/app/select.php
+++ src/app/select.php
@@ -3,28 +3,9 @@ use Respect\Validation\Validator as v;
 
 見る: {
     $app->get('/select', function() use ($app, $container) {
-        $app->render('select/index.html.twig');
+        $app->render('select/select.html.twig');
         })
         ->name('select')
     ;
 }
 
-他の: {
-    $app->map('/story-view', function() use ($app, $container) {
-        $input = $app->request()->post();
-        echo 'story view' . PHP_EOL;
-        var_dump($input);
-    })
-        ->name('story_view')
-        ->via('GET', 'POST')
-        ;
-
-    $app->map('/add', function() use ($app, $container) {
-        $input = $app->request()->post();
-        echo 'add' . PHP_EOL;
-        var_dump($input);
-    })
-        ->name('add')
-        ->via('GET', 'POST')
-        ;
-}
