fd1f27ebea74e04c131d3e38ae5cdefa007e00c9
nantyoku <syukatsusouth@gmail.com>
2013-08-26 11:03:17 +0900

    splitted others.php

---
+++ src/app/others.php
@@ -0,0 +1,22 @@
+<?php
+use Respect\Validation\Validator as v;
+
+他の: {
+    $app->map('/story-view', function() use ($app, $container) {
+        $input = $app->request()->post();
+        echo 'story view' . PHP_EOL;
+        var_dump($input);
+    })
+        ->name('story_view')
+        ->via('GET', 'POST')
+        ;
+
+    $app->map('/add', function() use ($app, $container) {
+        $input = $app->request()->post();
+        echo 'add' . PHP_EOL;
+        var_dump($input);
+    })
+        ->name('add')
+        ->via('GET', 'POST')
+        ;
+}
