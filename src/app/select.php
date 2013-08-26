3ae6f050051b23b7154c69455a82dad5cb51db04
nantyoku <syukatsusouth@gmail.com>
2013-08-26 19:19:44 +0900

    became able to parse frame tree

--- src/app/select.php
+++ src/app/select.php
@@ -28,30 +28,32 @@ function list_to_tree($list) {
    return $tree;
 }
 
+function tree_to_list_with_depth($tree) {
+    $list  = [];
+    $depth = 0;
+    inner($list, $tree, $depth);
+    return $list;
+}
+
+function inner(&$list, $tree, $depth) {
+    foreach ($tree as $node) {
+        if (!array_key_exists($depth, $list)) {
+            $list[$depth] = [];
+        }
+        array_push($list[$depth], [
+            'id'  => $node['id'],
+            'src' => $node['src']
+        ]);
+        
+        if (array_key_exists('kids', $node)) {
+            inner($list, $node['kids'], $depth+1);
+        }
+    }
+}
 
 見る: {
     $app->get('/select', function() use ($app, $container) {
         $theme_id = 1;
-
-        $first_frame = [
-            'src' => 'http://192.168.56.110:18003/img/plus.png',
-            'id'  => 1
-        ];
-
-        $frame_rows = [
-            [
-                ['src' => 'http://192.168.56.110:18003/img/plus.png',
-                 'id'  => 2],
-                ['src' => 'http://192.168.56.110:18003/img/plus.png',
-                 'id'  => 3]
-            ],
-            [
-                ['src' => 'http://192.168.56.110:18003/img/plus.png',
-                 'id'  => 4],
-                ['src' => 'http://192.168.56.110:18003/img/plus.png',
-                 'id'  => 5]
-            ],
-        ];
 
 //        $theme_repository = $container['repository.theme'];
 //        $theme = $theme_repository->findById($theme_id);
@@ -76,15 +78,19 @@ function list_to_tree($list) {
 //
 
         $frames = [
-            ['id' => 1, 'parent_id' => 0, 'src' => 'http://192.168.56.110:18003/img/plus.png'],
-            ['id' => 2, 'parent_id' => 1, 'src' => 'http://192.168.56.110:18003/img/plus.png'],
-            ['id' => 3, 'parent_id' => 1, 'src' => 'http://192.168.56.110:18003/img/plus.png']
+            ['id' => 1, 'parent_id' => 0, 'src' => '/img/plus.png'],
+            ['id' => 2, 'parent_id' => 1, 'src' => '/img/plus.png'],
+            ['id' => 3, 'parent_id' => 1, 'src' => '/img/plus.png'],
+            ['id' => 4, 'parent_id' => 3, 'src' => '/img/plus.png']
         ];
 
-        $frame_tree = list_to_tree($frames)['0'];
-        var_dump($frame_tree);
-
+        $frame_tree = list_to_tree($frames);
+        $frame_rows = tree_to_list_with_depth($frame_tree);
         array_push($frame_rows, []);
+
+        $first_frame = $frame_rows[0][0];
+        unset($frame_rows[0]);
+
         $app->render('select/select.html.twig', ['first_frame' => $first_frame, 'frame_rows' => $frame_rows]);
         })
         ->name('select')
