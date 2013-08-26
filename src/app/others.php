188aab1e87428dcb97afa9b3ccba650b838b7ed2
1000ch123 <masa.guruto@gmail.com>
2013-08-26 18:01:25 +0900

    remove dual definition

--- src/app/others.php
+++ src/app/others.php
@@ -2,13 +2,6 @@
 use Respect\Validation\Validator as v;
 
 他の: {
-    $app->get('/story-view', function() use ($app, $container) {
-        $input = $app->request()->get();
-        echo 'story view' . PHP_EOL;
-        var_dump($input);
-    })
-        ->name('story_view_get')
-        ;
 
     $app->get('/add', function() use ($app, $container) {
         $input = $app->request()->get();
