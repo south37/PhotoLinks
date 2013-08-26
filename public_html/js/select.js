d3ede802a928f3d1bc0b770baf5558b5a07903c9
nantyoku <syukatsusouth@gmail.com>
2013-08-26 14:08:06 +0900

    added border on clicked img

--- public_html/js/select.js
+++ public_html/js/select.js
@@ -2,8 +2,11 @@
 
 (function(){
     $('div.frames img').click( function () {
+        $(this).parent().children('img').css({'border': '0px solid #000000'});
+        $(this).css({'border': '2px solid #009999'});
+
         var selected_val = $(this).attr('value');
-        $(this).parent().parent().next('div').find('input').attr('value', selected_val);
+        $(this).parent().parent().next('div.frames').find('input').attr('value', selected_val);
 
         var frames = '-1';
         $('div.frames').each( function() {
