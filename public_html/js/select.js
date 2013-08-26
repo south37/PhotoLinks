fca2b33f5aa6d3d82e174284d240777496eea6e7
nantyoku <syukatsusouth@gmail.com>
2013-08-26 12:26:41 +0900

    in select.js, selected-frames-id made from above input boxed value

--- public_html/js/select.js
+++ public_html/js/select.js
@@ -1,12 +1,18 @@
 "use strict";
 
 (function(){
-    $('.frames img').click( function () {
+    $('div.frames img').click( function () {
         var selected_val = $(this).attr('value');
         $(this).parent().parent().next('div').find('input').attr('value', selected_val);
 
-        var frames_container = $('#story-fm [name=selected-frames-id]');
-        var frames = frames_container.val();
-        frames_container.val(frames + ',' + selected_val);
+        var frames = '-1';
+        $('div.frames').each( function() {
+            var val = $(this).find('input').attr('value');
+            if (val !== '') {
+                frames += ',';
+                frames += val;
+            }
+        });
+        $('#story-fm [name=selected-frames-id]').val(frames);
     });
 })();
