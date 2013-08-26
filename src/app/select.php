bc06592d777e7e8b30baa7063ab980f431025e0f
nantyoku <syukatsusouth@gmail.com>
2013-08-26 12:11:06 +0900

    modified test frame_rows in select.php

--- src/app/select.php
+++ src/app/select.php
@@ -4,15 +4,21 @@ use Respect\Validation\Validator as v;
 見る: {
     $app->get('/select', function() use ($app, $container) {
         $frame_rows = [
-            '0' => [
+            [
                 ['src' => 'http://192.168.56.110:18003/img/plus.png',
                  'id'  => 1]
             ],
-            '1' => [
+            [
                 ['src' => 'http://192.168.56.110:18003/img/plus.png',
                  'id'  => 2],
                 ['src' => 'http://192.168.56.110:18003/img/plus.png',
                  'id'  => 3]
+            ],
+            [
+                ['src' => 'http://192.168.56.110:18003/img/plus.png',
+                 'id'  => 4],
+                ['src' => 'http://192.168.56.110:18003/img/plus.png',
+                 'id'  => 5]
             ]
         ];
 
