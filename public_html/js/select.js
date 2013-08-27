68ab870a4e09b3e2049616c726d70cdbcf26a327
nantyoku <syukatsusouth@gmail.com>
2013-08-27 22:19:28 +0900

    changed parent_id to id

--- public_html/js/select.js
+++ public_html/js/select.js
@@ -5,16 +5,16 @@
         $(this).parent().children('img').css({'border': '0px solid #000000'});
         $(this).css({'border': '2px solid #009999'});
 
-        var selected_val = $(this).attr('value');
-        var next_div     = $('[parent-id=' + selected_val + ']');
-        var next_depth   = $(this).parent().parent().data('depth')+1;
+        var selected_id = $(this).attr('value');
+        var child_div   = $('#' + selected_id);
+        var next_depth  = $(this).parent().parent().data('depth')+1;
    
         var next_depth_divs = $('[data-depth=' + next_depth + ']');
         next_depth_divs.css({'display': 'none'});
         next_depth_divs.find('input').attr('value', '');
 
-        next_div.css({'display':'block'});
-        next_div.find('input').attr('value', selected_val);
+        child_div.css({'display':'block'});
+        child_div.find('input').attr('value', selected_id);
 
         var frames = '';
         var first = true;
