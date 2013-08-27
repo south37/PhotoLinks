3fcc73cb58be27724691bfba0728d3e3484d1f6d
1000ch123 <masa.guruto@gmail.com>
2013-08-27 16:36:32 +0900

    post new frame

--- src/Vg/Validator/AddFrame.php
+++ src/Vg/Validator/AddFrame.php
@@ -50,4 +50,3 @@ class AddFrame
         return $this->errors;
     }
 }
-}
--- src/app/add_frame.php
+++ src/app/add_frame.php
@@ -9,21 +9,23 @@
  * 
  */
 
-
-
-
-// 通常のget
-$app->get('/add_frame', function() use ($app) {
+use Respect\Validation\Validator as v;
+ 
+// get_from_view2
+$app->get('/add_frame', function() use ($app,$container) {
     $input = $app->request()->get();
     var_dump($input);
 
+    $container['session']->set('theme_id',1);
+    $container['session']->set('parent_id',$input['parent-id']);
+
     $image_id = -1;
-    $app->render('add_frame/add_frame.html.twig',["image_id"=>$image_id]);
+    $app->render('add_frame/add_frame.html.twig',["image_id" => $image_id]);
     })
     ->name('add_frame_from_select')
     ;
 
-//通常のget
+// geti_from_view5
 $app->get('/add_frame/:image_id', function($image_id) use ($app) {
     $input = $app->request()->get();
     echo $image_id;
@@ -34,27 +36,61 @@ $app->get('/add_frame/:image_id', function($image_id) use ($app) {
     ;
 
 
-// フレームを追加
-$app->post('/add_frame', function() use ($app) {
+// push_make_frame 
+$app->post('/add_frame/make_frame', function() use ($app,$container) { // form情報を取得 
     $input = $app->request()->post();
+    $input['last_story_id'] = 0;
+    $input['user_id'] = $container['session']->get('user.id');
+    $input['theme_id'] = $container['session']->get('theme_id');
+
     var_dump($input);
-    $app->render('add_frame/add_frame.html.twig');
+
+    // validation
+    $validator = new  \Vg\Validator\AddFrame();
+
+    if (!$validator->validate($input)){
+        $app->render('add_frame/add_frame.html.twig',['errors'=> $validator->errors(),'image_id'=>$input['image_id']]);
+        exit;
+    }
+
+    // makeFrame + addDB 
+    if (!makeFrame($input,$app,$container)){
+        exit;
+    }
+
+    $app->redirect($app->urlFor('select'));
 
 })->name('make_frame');
 
 
 // ストーリーを作成 
-$app->post('/add_frame', function() use ($app) {
+$app->post('/add_frame/make_story', function() use ($app) {
     $input = $app->request()->post();
     var_dump($input);
+    
     $app->render('add_frame/add_frame.html.twig');
 
 })->name('make_story');
 
 //フレーム追加
-function addFrame(){
+function makeFrame($property,$app,$c){
+   
+    $frame = new \Vg\Model\Frame();
     
+    $frame->setProperties($property);
 
-
+    $repository = $c['repository.frame'];
+    
+    try {
+        $repository->insert($frame);
+        return true;
+    } catch (Exception $e) {
+        $app->halt(500, $e->getMessage());
+        return false;
+    }
 };
 
+// 確認画面
+$app->get('/add_frame/confirm',function() use ($app){
+    $app->render('add_frame/add_frame_confirm.html.twig');
+})->name('add_frame_confirm');
