1dd03beefebff115ebc35e2ea21be5dc78c22688
nantyoku <syukatsusouth@gmail.com>
2013-08-27 10:59:58 +0900

    modified select ui

--- public_html/js/select.js
+++ public_html/js/select.js
@@ -6,7 +6,9 @@
         $(this).css({'border': '2px solid #009999'});
 
         var selected_val = $(this).attr('value');
-        $(this).parent().parent().next('div.frames').find('input').attr('value', selected_val);
+        var next_div = $(this).parent().parent().next('div.frames');
+        next_div.css({'display':'block'});
+        next_div.find('input').attr('value', selected_val);
 
         var frames = '';
         var first = true;
