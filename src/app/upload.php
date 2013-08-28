7c63069ce2a18099f172b77e4472f67474388c4e
nantyoku <syukatsusouth@gmail.com>
2013-08-28 14:22:09 +0900

    modified upload

--- src/app/upload.php
+++ src/app/upload.php
@@ -21,46 +21,54 @@ use Respect\Validation\Validator as v;
     ;
 
     $app->post('/upload', $redirectIfNotLogin($container['session']), function () use ($app,$container) {
-            if (is_uploaded_file($_FILES['upfile']['tmp_name'])) {
-                if(move_uploaded_file($_FILES['upfile']['tmp_name'],__DIR__.'/../../public_html/img/public_img/'.$_FILES['upfile']['name'])) {
-
-                    $image = new \Vg\Model\Image();
-                    $imagefile = [
-                            "user_id" => ($container['session']->get('user.id')),
-                            "path"    => "",
-                            "scope"   => 0,
-                            "deleted" => 0
-                        ]; 
-                    $image->setProperties($imagefile);
-                    $repository = $container['repository.image'];
-                    // 画像データをDBにInsert
-                     try {
-                        $image_id = $repository->insert($image);
-                    } catch (Exception $e) {
-                        $app->halt(500, $e->getMessage());
-                    }
-
-                    // image_idと画像の拡張子を取得してpathを更新
-                    $extension = pathinfo($_FILES['upfile']['name'], PATHINFO_EXTENSION);
-                    $imagefile = array("id"=>$image_id,"user_id"=>($container['session']->get('user.id')),"path"=>"/img/public_img/".$image_id.".".$extension,"scope"=>0,"deleted"=>0);
-                    $image->setProperties($imagefile);
-                     try {
-                        $repository->update($image);
-                    } catch (Exception $e) {
-                        $app->halt(500, $e->getMessage());
-                    }
-                    // リネーム処理
-                    rename(__DIR__.'/../../public_html/img/public_img/'.$_FILES['upfile']['name'],__DIR__."/../../public_html/img/public_img/".$image_id.".".$extension);
-
-                    $app->redirect($app->urlFor('add_frame_from_upload',array("image_id" => $image_id)));
-                } else {
-                    $app->flash('info', 'アップロード失敗。');
-                    $app->redirect($app->urlFor('upload_post'));
-                }
-            } else {
-                    $app->flash('info', 'アップロード失敗');
-                    $app->redirect($app->urlFor('upload_post'));
+            if (!is_uploaded_file($_FILES['upfile']['tmp_name'])) {
+                $app->flash('info', 'アップロード失敗');
+                $app->redirect($app->urlFor('upload_post'));
             }
+            
+            if(!move_uploaded_file($_FILES['upfile']['tmp_name'], __DIR__.'/../../public_html/img/public_img/'.$_FILES['upfile']['name'])) {
+                $app->flash('info', 'アップロード失敗');
+                $app->redirect($app->urlFor('upload_post'));
+            }
+
+            $image = new \Vg\Model\Image();
+            $imagefile = [
+                "user_id" => ($container['session']->get('user.id')),
+                "path"    => "",
+                "scope"   => 0,
+                "deleted" => 0
+                ]; 
+            $image->setProperties($imagefile);
+            $repository = $container['repository.image'];
+            
+            // 画像データをDBにInsert
+            try {
+                $image_id = $repository->insert($image);
+            } catch (Exception $e) {
+                $app->halt(500, $e->getMessage());
+            }
+
+            // image_idと画像の拡張子を取得してpathを更新
+            $extension = pathinfo($_FILES['upfile']['name'], PATHINFO_EXTENSION);
+            $imagefile = [
+                "id"      => $image_id,
+                "user_id" => ($container['session']->get('user.id')),
+                "path"    => "/img/public_img/" . $image_id . "." . $extension,
+                "scope"   => 0,
+                "deleted" => 0
+            ];
+            $image->setProperties($imagefile);
+            try {
+                $repository->update($image);
+            } catch (Exception $e) {
+                $app->halt(500, $e->getMessage());
+            }
+
+            // リネーム処理
+            rename(__DIR__.'/../../public_html/img/public_img/'.$_FILES['upfile']['name'],
+                   __DIR__."/../../public_html/img/public_img/".$image_id.".".$extension);
+
+            $app->redirect($app->urlFor('add_frame_from_upload', ["image_id" => $image_id]));
         })
         ->name('upload_post')
     ;
