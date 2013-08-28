ba48dbb06d6be1292eaabf40ed31cdf34a1a0bb3
nantyoku <syukatsusouth@gmail.com>
2013-08-28 11:06:17 +0900

    modified material

--- src/app/material.php
+++ src/app/material.php
@@ -1,17 +1,17 @@
 <?php
 
 素材: {
-    $app->get('/material', function() use ($app, $container) {
-        $images = $container['repository.image']->findByPage(0);
+    $app->get('/material/:page', function($page) use ($app, $container) {
+        $images = $container['repository.image']->findByPage($page-1);
         $image_array = [];
         foreach ($images as $image) {
             array_push($image_array, [
-                'id' => $image->id,
+                'id'  => $image->id,
                 'src' => $image->path
             ]);
         }
         
-        $app->render('material/material.html.twig', ['images' => $image_array]);
+        $app->render('material/material.html.twig', ['images' => $image_array, 'page' => $page]);
     })
         ->name('material')
         ;
